0c68f4bf031673f2d766b6f2ba1a6dc0dca74605
Morgan Riffer <technologyriff@gmail.com>
2015-07-16 10:10:40 -0400

    images and categories

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   get 'contact' => 'welcome#contact'
 
+  get 'present1' => 'welcome#present1'
+  get 'present2' => 'welcome#present2'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
